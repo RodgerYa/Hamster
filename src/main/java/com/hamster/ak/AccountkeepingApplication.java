d3f421040812035340395367b7e0299bfeaec3ed
闫文博 <yanwenbo@yanwenbodeMacBook-Pro.local>
2018-06-22 08:53:51 +0800

    init hamster project

---
+++ src/main/java/com/hamster/ak/AccountkeepingApplication.java
@@ -0,0 +1,12 @@
+package com.hamster.ak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountkeepingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AccountkeepingApplication.class, args);
+    }
+}
